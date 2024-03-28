dda81bfd3bc0bcfdb8a71a1b184f7c100d6bfbd0
Jay Thanki <34959099+jaythanki0909@users.noreply.github.com>
2024-03-27 23:07:35 -0600

    added new url to cors in settings

--- back-end/script_synergy_backend/script_synergy_backend/settings.py
+++ back-end/script_synergy_backend/script_synergy_backend/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = ["http://cs872-script-synergy.vercel.app/","https://script-synergy-six.vercel.app/"]
+CORS_ALLOWED_ORIGINS = ["https://cs872-script-synergy.vercel.app","https://script-synergy-six.vercel.app"]
 
 # Application definition
 
